e26b6a86fcbfcd9d3d7fa08a46e495ed734c657b
anaafie <bw.anaafie@gmail.com>
2019-12-23 23:24:21 +0700

    action list

--- main.py
+++ main.py
@@ -10,14 +10,24 @@ def root():
 
 @app.get("/division-list")
 def read_item():
-    return [{"Id": "1", "name": "EVP RKO"},
-    {"Id": "2", "name": "ADT Commerce and CS"},
-    {"Id": "3", "name": "EVP Kons Reg JBT"}]
+    return [
+        {"Id": "1", "name": "EVP RKO"},
+        {"Id": "2", "name": "ADT Commerce and CS"},
+        {"Id": "3", "name": "EVP Kons Reg JBT"}
+    ]
 
 @app.get("/employee-list")
 def read_item():
     return [
-    {"Id": "1", "name": "Achmad"},
-    {"Id": "2", "name": "Budi"},
-    {"Id": "3", "name": "Caca"},
+        {"Id": "1", "name": "Achmad"},
+        {"Id": "2", "name": "Budi"},
+        {"Id": "3", "name": "Caca"}
+    ]
+
+@app.get("/action-list")
+def read_item():
+    return [
+        {"Id": "1", "name": "Approve"},
+        {"Id": "2", "name": "Reject"},
+        {"Id": "3", "name": "Return"},
     ]
