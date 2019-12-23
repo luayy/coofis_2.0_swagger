ac0b845a00673ca6ee599c7c1a402233d4c7351b
anaafie <bw.anaafie@gmail.com>
2019-12-24 02:04:19 +0700

    message detail

--- routers/inbox.py
+++ routers/inbox.py
@@ -3,8 +3,12 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get("/inbox/")
-def read_inbox():
+def read_inboxes():
     return [
-        {"Id": "1", "sender": "Budi", "receiver": "Indah", "date": "24/12/2019", "body": "Tolong segera adakan kegiatan donor darah"},
-        {"Id": "2", "sender": "Eko", "receiver": "Achmad", "date": "23/12/2019", "body": "Pmeberitahuan cuti bersama Natal dan Tahun Baru 2019"},
+        {"nomor_surat": "32/AGA/None/2019-R", "sender": "Budi", "receiver": "Indah", "date": "24/12/2019", "body": "Tolong segera adakan kegiatan donor darah"},
+        {"nomor_surat": "24/EVP/None/2019-R", "sender": "Eko", "receiver": "Achmad", "date": "23/12/2019", "body": "Pmeberitahuan cuti bersama Natal dan Tahun Baru 2019"},
     ]
+
+@router.get("/inbox/{nomor_surat}")
+def read_inbox(id_surat: str):
+    return {"body": "some important mail", "nomor_surat": id_surat}
