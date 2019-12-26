af287b584f3f5c6f50b546cc32ba57be05aeca16
anaafie <bw.anaafie@gmail.com>
2019-12-26 12:03:40 +0700

    get priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, FastAPI, Header, HTTPException
 from pydantic import BaseModel
-from routers import divisions, employees, actions, inbox, profile
+from routers import divisions, employees, actions, inbox, profile, priority
 
 app = FastAPI()
 
@@ -9,3 +9,4 @@ app.include_router(employees.router)
 app.include_router(actions.router)
 app.include_router(inbox.router)
 app.include_router(profile.router)
+app.include_router(priority.router)
