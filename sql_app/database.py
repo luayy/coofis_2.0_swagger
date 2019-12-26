43a5c2d8b529827dbe1e12c9cc10fb442dcd2086
anaafie <bw.anaafie@gmail.com>
2019-12-26 14:02:33 +0700

    init models

--- sql_app/database.py
+++ sql_app/database.py
@@ -4,9 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/coofis2_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
