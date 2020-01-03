29f75b445c6e8f901a58eb43759c00f28d3df3b8
anaafie <bw.anaafie@gmail.com>
2020-01-03 22:17:56 +0700

    inbox tags in swagger

--- routers/inbox.py
+++ routers/inbox.py
@@ -16,24 +16,24 @@ class Inbox(BaseModel):
     type: str
     desc: str
 
-@router.get("/nde/inbox/")
+@router.get("/nde/inbox/", tags=["Inbox"])
 def get_inboxes():
     return [
         {"nomor_surat": "32/AGA/None/2019-R", "sender": "Budi", "receiver": "Indah", "date": "24/12/2019", "body": "Tolong segera adakan kegiatan donor darah"},
         {"nomor_surat": "24/EVP/None/2019-R", "sender": "Eko", "receiver": "Achmad", "date": "23/12/2019", "body": "Pmeberitahuan cuti bersama Natal dan Tahun Baru 2019"},
     ]
 
-@router.get("/nde/inbox/{nomor_surat}")
+@router.get("/nde/inbox/{nomor_surat}", tags=["Inbox"])
 def get_inbox_detail(id_surat: str):
     return {"body": "some important mail", "nomor_surat": id_surat}
 
-@router.get("/nde/inbox/send/")
+@router.get("/nde/inbox/send/", tags=["Inbox"])
 def get_inbox_send():
     return [
         {"nomor_surat" : "01/CS/None/2019-R", "sender": "Fachrul",
         "receiver" : ["Indah", "Gading", "Bruno"], "date": "25/12/2019", "body": "Segera adakan agenda rapat bulanan"}
     ]
 
-@router.post("/nde/inbox/")
+@router.post("/nde/inbox/", tags=["Inbox"])
 async def create_inbox(inbox:Inbox):
     return inbox
